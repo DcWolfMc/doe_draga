6c16418f0c44a8bae04fd26d38a9fa2c8ead463b
Daniel Colares <danielmcolares@gmail.com>
2022-11-07 22:17:01 -0300

    07/11/2022 - commit

--- src/pages/NewAnnounce/index.tsx
+++ src/pages/NewAnnounce/index.tsx
@@ -1,9 +1,19 @@
-import React, { FunctionComponent, useState,ChangeEvent, ChangeEventHandler, FormEvent } from "react";
+import React, {
+  FunctionComponent,
+  useState,
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEvent,
+} from "react";
 import { CustomCheckbox } from "../../components/CustomCheckbox/CustomCheckbox";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { addDays } from "date-fns";
+import { useNavigate } from "react-router-dom"
+import { v4 as uuidv4 } from "uuid";
+import { Announce } from "../../@types/Announce";
 import {
   Container,
   FormContainer,
@@ -23,40 +33,66 @@ import {
   InputTypeTextFlex,
   TextArea,
 } from "./styles";
-import { StyledDropzone } from "../../components/StyledDropzone";
 
-interface Anuncio {
-  id: string;
-  email: string;
-  nome_criador: string;
-  telefone: string;
-  data_criacao: Date;
-  data_liberacao: Date;
-  data_termino: Date;
-  duracao?: number;
-  tipo_pix: string;
-  whats_app_link?: string;
-  titulo: string;
-  texto: string[];
-  imagem?: string;
-  status: "ativo" | "encerrado" | "analise";
-}
+import { createAnnounce } from "../../services/api";
 
 export const NewAnnounce: FunctionComponent = () => {
+  const navigation = useNavigate()
   const [nomeCriador, setNomeCriador] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [telefone, setTelefone] = useState<string>("");
   const [adTitle, setAdTitle] = useState<string>("");
   const [adContent, setAdContent] = useState<string>("");
-  const [isStartSpecific, setIsStartSpecific] = useState<boolean>(false);
+  const [StartSpecificDate, setStartSpecificDate] = useState<boolean>(false);
+  const [initialDate, setInitialDate] = useState<string>("");
   const [isEndSpecific, setIsEndSpecific] = useState<boolean>(false);
-  const [pixKey, setPixKey] = useState<string>('');
-  const [pix, setPix] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>("");
+  const [pixKey, setPixKey] = useState<string>("");
   const [image, setImage] = useState<[]>([]);
+  const [duration, setDuration] = useState<number>();
 
-  function handleSubmit(e:FormEvent) {
-    e.preventDefault
-    
+  let teste:Announce = {
+    id: "0",
+    email: email,
+    nome_criador: nomeCriador,
+    telefone: telefone,
+    titulo: adTitle,
+    texto: adContent,
+    status: "analise",
+    pixKey: pixKey,
+    data_liberacao: new Date(initialDate),
+    data_termino: new Date(endDate),
+    duracao: duration,
+  }
+
+  console.log(teste);
+  
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    let announce: Announce = {
+      id: uuidv4(),
+      email: email,
+      nome_criador: nomeCriador,
+      telefone: telefone,
+      titulo: adTitle,
+      texto: adContent,
+      status: "analise",
+      pixKey: pixKey,
+    };
+    isEndSpecific
+      ? (announce = { ...announce, data_termino: new Date(endDate) })
+      : (announce = { ...announce, duracao: duration });
+    StartSpecificDate
+      ? (announce = { ...announce, data_liberacao: new Date(initialDate) })
+      : (announce = announce);
+      console.log("announce:",announce);
+      
+      createAnnounce(announce).then(res=>{
+        console.log(res);
+        navigation("/adList")
+        
+      })
   }
 
   return (
@@ -71,16 +107,22 @@ export const NewAnnounce: FunctionComponent = () => {
                   required
                   type="text"
                   placeholder="Nome do Anunciante"
+                  value={nomeCriador}
+                  onChange={(event)=>{setNomeCriador(event.target.value)}}
                 />
                 <InputTypeTextFlex
                   required
-                  type="text"
+                  type="email"
                   placeholder="E-mail de contato"
+                  value={email}
+                  onChange={(event)=>{setEmail(event.target.value)}}
                 />
                 <InputTypeTextFlex
                   required
                   type="text"
                   placeholder="Telefone com DDD"
+                  value={telefone}
+                  onChange={(event)=>setTelefone(event.target.value)}
                 />
                 <p>
                   Essas informações são importantes para que possamos validar a
@@ -96,17 +138,22 @@ export const NewAnnounce: FunctionComponent = () => {
                     Deseja iniciar o anúncio em alguma data em específico?
                   </label>
                   <CustomCheckbox
-                    checked={isStartSpecific}
-                    onChange={() => setIsStartSpecific(true)}
+                    checked={StartSpecificDate}
+                    onChange={() => setStartSpecificDate(true)}
                     label="Sim"
                   />
                   <CustomCheckbox
-                    checked={!isStartSpecific}
-                    onChange={() => setIsStartSpecific(false)}
+                    checked={!StartSpecificDate}
+                    onChange={() => setStartSpecificDate(false)}
                     label="Não"
                   />
                 </CheckBoxWrapper>
-                <InputTypeTextFlex type={"date"} />
+                <InputTypeTextFlex
+                  disabled={!StartSpecificDate}
+                  type={"date"}
+                  value={initialDate}
+                  onChange={(event)=>setInitialDate(event.target.value)}
+                />
 
                 <div>
                   <label>
@@ -125,9 +172,14 @@ export const NewAnnounce: FunctionComponent = () => {
                     />
                   </CheckBoxWrapper>
                   {isEndSpecific ? (
-                    <InputTypeTextFlex type={"date"} />
+                    <InputTypeTextFlex type={"date"} value={endDate} onChange={(event)=>setEndDate(event.target.value)}/>
                   ) : (
-                    <InputTypeTextFlex type={"text"} />
+                    <InputTypeTextFlex
+                      type={"number"}
+                      placeholder="tempo de duração do anúncio (DIAS)"
+                      value={duration}
+                      onChange={(event)=>setDuration(Number(event.target.value))}
+                    />
                   )}
                 </div>
               </div>
@@ -141,43 +193,41 @@ export const NewAnnounce: FunctionComponent = () => {
                   <label>
                     Deseja adicionar alguma imagem para o seu anúncio ?
                   </label>
-                  <InputFile type={"file"} name="" id="" />
+                  <InputFile type="file" name="" id="" />
                 </div>
               </AdImageWrapper>
               <AdTitleAndHistory>
-                <InputTypeTextFlex type="text" />
+                <InputTypeTextFlex
+                  type="text"
+                  placeholder="Titulo do Anúncio"
+                  value={adTitle}
+                  onChange={event=>setAdTitle(event.target.value)}
+                />
 
                 <TextArea
                   placeholder={"Conte sua história"}
                   rows={10}
-                  onChange={(e:ChangeEvent<HTMLTextAreaElement>) => setAdContent(e.target.value)}
+                  onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                    setAdContent(e.target.value)
+                  }
                 />
               </AdTitleAndHistory>
             </AdFormContentFields>
           </AdFormContentWrapper>
         </AdFormWrapper>
         <DonationFormWrapper>
-          <TitleText>
-            Informe para a doação
-            </TitleText>
-            <div>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Chave Pix</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={pixKey}
-                  label="Age"
-                  onChange={(e: SelectChangeEvent)=>{setPixKey(e.target.value)}}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              <InputTypeTextFlex value={pix} onChange={(e:ChangeEvent<HTMLInputElement>)=>{setPix (e.target.value)}} type="text" placeholder="Pix" />
-            </div>
-            <LinkButton type="submit">Finalizar Cadastro</LinkButton>
+          <TitleText>Informe para a doação</TitleText>
+          <div>
+            <InputTypeTextFlex
+              value={pixKey}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setPixKey(e.target.value);
+              }}
+              type="text"
+              placeholder="Chave PIX do que receberá o dinheiro"
+            />
+          </div>
+          <LinkButton type="submit">Finalizar Cadastro</LinkButton>
         </DonationFormWrapper>
       </FormContainer>
     </Container>
